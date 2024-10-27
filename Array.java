8332d7d33563be5ca2cc27d8b81eb7be9e5713ec
tshelar <tshelar@asu.edu>
2024-10-27 00:41:14 -0700

    Trying to fork with personal account.

---
+++ Array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Array {
+//Taking user input
+    static String userInput(){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter car name:");
+        String carName = myObj.nextLine();
+        return carName;
+    }
+    public static void main (String[]args){
+    String[] cars = {"Volvo", "Mercedes", "BMW", "Ford", "Tata", " Ferrari"};
+    for(int i=0 ; i<=5;i++){
+        System.out.println(cars[i]);
+    }
+    String carName = userInput();
+    System.out.println(userInput());
+}
+}
